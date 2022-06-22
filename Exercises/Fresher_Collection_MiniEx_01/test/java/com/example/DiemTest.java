f0d61a61d64dc746c2dc0748ca43187b8edba2fa
thuyntb2022 <100392994+thuyntb2022@users.noreply.github.com>
2022-06-22 17:45:27 +0700

    Add files via upload

---
+++ Exercises/Fresher_Collection_MiniEx_01/test/java/com/example/DiemTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiemTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testClone() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+}
---
+++ Exercises/Fresher_Collection_MiniEx_01/test/java/com/example/MonHocTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonHocTest {
+
+    @Test
+    void testToString() {
+        MonHoc mh = new MonHoc("GT",1,1);
+    }
+}
